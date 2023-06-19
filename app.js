4ce76d02d762bd2e2ea7b25cc8d7cf2e451b2102
VictoriyaK19 <kalinovavictoriya@gmail.com>
2023-06-19 20:11:57 +0300

    working with watchers

--- app.js
+++ app.js
@@ -2,16 +2,42 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name: ''
+      name: '',
+      lastName: '',
+      // fullname: ''
     };
+  },
+  watch: {
+    counter(value) {
+      const that = this;
+      if (value > 50) {
+        setTimeout(function() {
+          that.counter = 0;
+        }, 2000)
+      }
+    }
+    // name(value) {
+    //   if (value === '') {
+    //     this.fullname = '';
+    //   } else {
+    //     this.fullname = value + ' ' + this.lastName;
+    //   }
+    // },
+    // lastName(value) {
+    //   if (value === '') {
+    //     this.fullname = '';
+    //   } else {
+    //     this.fullname = this.name + ' ' + value;
+    //   }
+    // },
   },
   computed: {
     fullname() {
       console.log('Running again');
-      if (this.name === '') {
+      if (this.name === '' || this.lastName === '') {
         return '';
       }
-      return this.name + ' ' + 'Kalinova';
+      return this.name + ' ' + this.lastName;
     }
   },
   methods: {
